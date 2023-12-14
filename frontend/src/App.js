2fd2256f880d175ae3e58cd0bef6bb27548b7882
Can Kayalı <volkand135@gmail.com>
2023-12-14 21:04:12 +0300

    Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import { ChatEngine } from 'react-chat-engine';
-import './App.css';
+
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '162ac88f-44a4-432d-95c5-3c8911def152';
 
 const App = () => {
-    return (
-        <ChatEngine
-            height="100vh"
-            projectID="162ac88f-44a4-432d-95c5-3c8911def152"
-            userName="Can"
-            userSecret="123123"
-            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-        />
-    ); 
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
 
 export default App;
